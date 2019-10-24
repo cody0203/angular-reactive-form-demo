674ae37be5acf3fbacc75dff1ed01ee81eb97cd0
Cody <cody@Thuy.local>
2019-10-24 11:36:06 +0700

    remove console log data

--- src/app/submit-data/submit-data.component.ts
+++ src/app/submit-data/submit-data.component.ts
@@ -1,17 +1,13 @@
-import { Component, Input, OnInit } from "@angular/core";
+import { Component, Input } from "@angular/core";
 
 @Component({
   selector: "app-submit-data",
   templateUrl: "./submit-data.component.html",
   styleUrls: ["./submit-data.component.css"]
 })
-export class SubmitDataComponent implements OnInit {
+export class SubmitDataComponent {
   @Input() submitData;
   address = ['Tỉnh/Thành phố', 'Quận/Huyện', 'Phường/Xã', 'Địa chỉ']
 
-  sortNull() {}
-
-  ngOnInit() {
-    console.log(this.submitData);
-  }
+  sortNull(): any {}
 }
